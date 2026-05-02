aa645e2f14430354332d01a6d11cb945179c5a8e
Harinarayan S <harinarayansubu@gmail.com>
2026-05-02 09:57:13 +0530

    updated

--- frontend/app/agents/page.tsx
+++ frontend/app/agents/page.tsx
@@ -2,5 +2,4 @@ import AIAgentsDashboard from '../../components/AIAgentsDashboard'
 
 export default function AgentsPage() {
   return <AIAgentsDashboard />
-}</content>
-<parameter name="filePath">e:\Projects_Local_Machine\stitch_aura_knowledge_os\stitch_aura_knowledge_os\app\agents\page.tsx
+}
